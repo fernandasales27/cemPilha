da0f93e769e4618d37edc2c2c2bb7756ed17ac82
Ricardo Lins <ricardinho200215@gmail.com>
2025-11-30 12:37:15 -0300

    fix: fix the ExpTop

--- Funcional1/src/lf1/plp/functional1/expression/ExpTop.java
+++ Funcional1/src/lf1/plp/functional1/expression/ExpTop.java
@@ -2,61 +2,52 @@ package lf1.plp.functional1.expression;
 
 import lf1.plp.expressions1.util.Tipo;
 import lf1.plp.functional1.util.TipoPilha;
-import lf1.plp.expressions2.expression.Expressao;
 import lf1.plp.expressions2.expression.ExpUnaria;
 import lf1.plp.expressions2.expression.Valor;
 import lf1.plp.expressions2.expression.ValorPilha;
+import lf1.plp.expressions2.expression.Expressao;
 import lf1.plp.expressions2.memory.AmbienteCompilacao;
 import lf1.plp.expressions2.memory.AmbienteExecucao;
-import lf1.plp.expressions2.memory.VariavelJaDeclaradaException;
 import lf1.plp.expressions2.memory.VariavelNaoDeclaradaException;
+import lf1.plp.expressions2.memory.VariavelJaDeclaradaException;
 
-public class ExpTop implements Expressao {
-
-    private Expressao expPilha;
+public class ExpTop extends ExpUnaria {
 
     public ExpTop(Expressao expPilha) {
-        this.expPilha = expPilha;
+        super(expPilha, "top");
     }
 
-    public Valor avaliar(AmbienteExecucao ambiente) 
+    @Override
+    public Valor avaliar(AmbienteExecucao ambiente)
             throws VariavelNaoDeclaradaException, VariavelJaDeclaradaException {
-        
-        ValorPilha pilha = (ValorPilha) expPilha.avaliar(ambiente);
-        
-        // Retorna o elemento no topo (não modifica a pilha)
-        return pilha.top();
+
+        ValorPilha pilha = (ValorPilha) getExp().avaliar(ambiente);
+        return pilha.top(); // não modifica a pilha
     }
 
-    public boolean checaTipo(AmbienteCompilacao ambiente) 
+    @Override
+    protected boolean checaTipoElementoTerminal(AmbienteCompilacao ambiente)
             throws VariavelNaoDeclaradaException, VariavelJaDeclaradaException {
-        
-        boolean tipoOk = expPilha.checaTipo(ambiente);
-        if (!tipoOk) return false;
 
-        Tipo tipoPilha = expPilha.getTipo(ambiente);
+        Tipo tipoPilha = getExp().getTipo(ambiente);
 
         if (!(tipoPilha instanceof TipoPilha)) {
-            System.err.println("Erro de Tipo: 'top' esperado em um stack. Encontrado: " + tipoPilha.getNome());
+            System.err.println("Erro de Tipo: 'top' esperado em uma pilha. Encontrado: " + tipoPilha.getNome());
             return false;
         }
         return true;
     }
 
-    public Tipo getTipo(AmbienteCompilacao ambiente) 
+    @Override
+    public Tipo getTipo(AmbienteCompilacao ambiente)
             throws VariavelNaoDeclaradaException, VariavelJaDeclaradaException {
-        
-        // Top retorna o tipo do ELEMENTO da pilha
-        TipoPilha tipoPilha = (TipoPilha) expPilha.getTipo(ambiente);
-        return tipoPilha.getSubTipo();
+
+        TipoPilha tipoPilha = (TipoPilha) getExp().getTipo(ambiente);
+        return tipoPilha.getSubTipo(); // Retorna o tipo do elemento da pilha
     }
 
-    public Expressao reduzir(AmbienteExecucao ambiente) {
-        this.expPilha = this.expPilha.reduzir(ambiente);
-        return this;
-    }
-    
+    @Override
     public ExpTop clone() {
-		return new ExpTop(expPilha.clone());
-	}
+        return new ExpTop(getExp().clone());
+    }
 }
